55681f183878d0819ced7995d1ffc864b18fa75e
France! <franciscodomingosjose06798@gmail.com>
2024-06-07 15:40:25 +0100

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/my_auths_firebase_ao/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.my_auths_firebase_ao
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/presenter/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+main(){
+  runApp(MyWidget());
+}
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
